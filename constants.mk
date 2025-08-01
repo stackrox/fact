b0f6c2af2d8e5ae47f659b61dbadd3a28d7576de
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-08-01 18:15:06 +0200

    Simplify constants.mk and minor cleanup

--- constants.mk
+++ constants.mk
@@ -1,7 +1,2 @@
-ifeq ($(FACT_TAG),)
-FACT_TAG=$(shell git describe --always --tags --abbrev=10 --dirty)
-endif
-
-ifeq ($(FACT_IMAGE_NAME),)
-FACT_IMAGE_NAME=quay.io/rhacs-eng/fact:$(FACT_TAG)
-endif
+FACT_TAG ?= $(shell git describe --always --tags --abbrev=10 --dirty)
+FACT_IMAGE_NAME ?= quay.io/rhacs-eng/fact:$(FACT_TAG)
