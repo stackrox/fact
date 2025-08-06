103d561751482082178c2c15df4c22cec9193ed0
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-08-06 16:30:33 +0200

    ROX-30421: Create a bpf bootstrap test
    The newly added test focuses solely in the bpf implementation, bypassing
    any additional higher level logic that might be added in the future.
    
    In order to achieve this, the BPF worker task is re-worked to use a
    broadcast channel, which should also allow us to implement extendable
    outputs by simply having subscriber tasks that will retrieve events from
    the same broadcast channel.
    
    The only test implemented thus far simply runs the BPF worker monitoring
    a temporary directory, creates a file in said directory and checks the
    generated event as best as possible.

--- fact/src/lib.rs
+++ fact/src/lib.rs
@@ -1,9 +1,11 @@
+use std::sync::Arc;
+
 use anyhow::Context;
 use bpf::Bpf;
 use log::{info, debug};
 use tokio::{
     signal::unix::{signal, SignalKind},
-    sync::watch::channel,
+    sync::{broadcast, watch},
 };
 
 mod bpf;
@@ -19,7 +21,8 @@ use config::FactConfig;
 use pre_flight::pre_flight;
 
 pub async fn run(config: FactConfig) -> anyhow::Result<()> {
-    let (tx, rx) = channel(true);
+    let (run_tx, run_rx) = watch::channel(true);
+    let (output_tx, mut output_rx) = broadcast::channel(100);
 
     if !config.skip_pre_flight {
         debug!("Performing pre-flight checks");
@@ -37,14 +40,37 @@ pub async fn run(config: FactConfig) -> anyhow::Result<()> {
     }
 
     // Create the gRPC client
-    let client = if let Some(url) = config.url.as_ref() {
+    let mut client = if let Some(url) = config.url.as_ref() {
         Some(Client::start(url, config.certs)?)
     } else {
         None
     };
 
+    let mut running = run_rx.clone();
+
+    tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                event = output_rx.recv() => {
+                    let event = Arc::unwrap_or_clone(event.expect("Failed to receive event"));
+
+                    println!("{event:?}");
+                    if let Some(client) = client.as_mut() {
+                         client.send(event).await.unwrap();
+                    }
+                },
+                _ = running.changed() => {
+                    if !*running.borrow() {
+                        info!("Stopping output worker...");
+                        return;
+                    }
+                }
+            }
+        }
+    });
+
     // Gather events from the ring buffer and print them out.
-    Bpf::start_worker(client, bpf.fd, config.paths, rx.clone());
+    Bpf::start_worker(output_tx, bpf.fd, config.paths, run_rx.clone());
 
     let mut sigterm = signal(SignalKind::terminate())?;
     tokio::select! {
@@ -52,7 +78,7 @@ pub async fn run(config: FactConfig) -> anyhow::Result<()> {
         _ = sigterm.recv() => {}
     }
 
-    tx.send(false)?;
+    run_tx.send(false)?;
     info!("Exiting...");
 
     Ok(())
