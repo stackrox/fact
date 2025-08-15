2f90089852e44d39795160fb28908cc9fd4befdc
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-08-15 10:48:16 +0200

    Add integration tests on CI (#4)
    * test: Add basis to run integration tests on GCP
    
    * Run integration tests on GCP
    
    * Generate logs and test results and upload them as artifacts
    
    * Add error to test for checking handling of errors on CI
    
    * Revert "Add error to test for checking handling of errors on CI"
    
    This reverts commit 0d99ef6cbaaa9b0c47ed0ac641c402a7b1ff5a30.
    
    * Simplify constants.mk and minor cleanup
    
    * Rename logs fixture to logs_dir

--- Makefile
+++ Makefile
@@ -1,10 +1,24 @@
+include constants.mk
+
+tag:
+	@echo "$(FACT_TAG)"
+
+image-name:
+	@echo "$(FACT_IMAGE_NAME)"
+
 mock-server:
 	make -C mock-server
 
 image:
-	docker build -f Containerfile -t fact:latest .
+	docker build \
+		-f Containerfile \
+		-t $(FACT_IMAGE_NAME) \
+		$(CURDIR)
 
 integration-tests:
 	make -C tests
 
-.PHONY: mock-server integration-tests image
+clean:
+	make -C tests clean
+
+.PHONY: tag mock-server integration-tests image image-name clean
--- tests/conftest.py
+++ tests/conftest.py
@@ -46,7 +46,30 @@ def server():
 
 
 @pytest.fixture
-def fact(docker_client, temp_dir, server):
+def logs_dir(request):
+    logs = os.path.join(os.getcwd(), 'logs', request.node.name)
+    os.makedirs(logs, exist_ok=True)
+    return logs
+
+
+@pytest.fixture(scope='session', autouse=True)
+def get_image(request, docker_client):
+    tag = request.config.getoption('--tag')
+    image = f'quay.io/rhacs-eng/fact:{tag}'
+    try:
+        docker_client.images.get(image)
+    except docker.errors.ImageNotFound:
+        docker_client.images.pull(image)
+
+
+def dump_logs(container, file):
+    logs = container.logs().decode('utf-8')
+    with open(file, 'w') as f:
+        f.write(logs)
+
+
+@pytest.fixture
+def fact(request, docker_client, temp_dir, server, logs_dir):
     """
     Run the fact docker container for integration tests.
     """
@@ -55,8 +78,9 @@ def fact(docker_client, temp_dir, server):
         '-p', temp_dir,
         '--health-check',
     ]
+    tag = request.config.getoption('--tag')
     container = docker_client.containers.run(
-        'fact:latest',
+        f'quay.io/rhacs-eng/fact:{tag}',
         command=command,
         detach=True,
         environment={
@@ -75,8 +99,13 @@ def fact(docker_client, temp_dir, server):
                 'bind': '/host/etc',
                 'mode': 'ro',
             },
+            '/proc/sys/kernel': {
+                'bind': '/host/proc/sys/kernel',
+                'mode': 'ro',
+            },
         },
     )
+    log_file = os.path.join(logs_dir, 'fact.log')
 
     # Wait for container to be ready
     for _ in range(3):
@@ -89,7 +118,7 @@ def fact(docker_client, temp_dir, server):
         sleep(1)
     else:
         container.stop(timeout=1)
-        print(container.logs().decode('utf-8'))
+        dump_logs(container, log_file)
         container.remove()
         pytest.fail('fact failed to start')
 
@@ -97,8 +126,7 @@ def fact(docker_client, temp_dir, server):
 
     container.stop(timeout=1)
     exit_status = container.wait(timeout=1)
-    logs = container.logs().decode('utf-8')
-    print(logs)
+    dump_logs(container, log_file)
     container.remove()
     assert exit_status['StatusCode'] == 0
 
@@ -110,3 +138,8 @@ def executor():
     in tests.
     """
     return futures.ThreadPoolExecutor(max_workers=2)
+
+
+def pytest_addoption(parser):
+    parser.addoption('--tag', action='store', default='latest',
+                     help='The tag to be used for testing')
