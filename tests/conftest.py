bd1cc0a25a5854a2e953a020038827e307f6f592
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-08-27 15:45:48 +0200

    chore: Push upstream images to quay.io/stackrox-io/fact
    This is related to the efforts to build fact on konflux. After some
    discussion, we have made the decision to push upstream images (those
    built on GHA) to quay.io/stackrox-io/fact and quay.io/rhacs-eng/fact
    will hold the downstream images (konflux built)

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,8 +54,7 @@ def logs_dir(request):
 
 @pytest.fixture(scope='session', autouse=True)
 def get_image(request, docker_client):
-    tag = request.config.getoption('--tag')
-    image = f'quay.io/rhacs-eng/fact:{tag}'
+    image = request.config.getoption('--image')
     try:
         docker_client.images.get(image)
     except docker.errors.ImageNotFound:
@@ -78,9 +77,9 @@ def fact(request, docker_client, temp_dir, server, logs_dir):
         '-p', temp_dir,
         '--health-check',
     ]
-    tag = request.config.getoption('--tag')
+    image = request.config.getoption('--image')
     container = docker_client.containers.run(
-        f'quay.io/rhacs-eng/fact:{tag}',
+        image,
         command=command,
         detach=True,
         environment={
@@ -141,5 +140,5 @@ def executor():
 
 
 def pytest_addoption(parser):
-    parser.addoption('--tag', action='store', default='latest',
-                     help='The tag to be used for testing')
+    parser.addoption('--image', action='store', default='quay.io/stackrox-io/fact:latest',
+                     help='The image to be used for testing')
