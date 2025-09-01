0085a46140fa1728c6ad1f8863a0f0f212f5c398
Giles Hutton <ghutton@redhat.com>
2025-09-01 16:24:21 +0100

    Adds multiarch manifest

--- constants.mk
+++ constants.mk
@@ -1,4 +1,3 @@
-FACT_ARCH ?= amd64
 FACT_TAG ?= $(shell git describe --always --tags --abbrev=10 --dirty)
 FACT_REGISTRY ?= quay.io/stackrox-io/fact
-FACT_IMAGE_NAME ?= $(FACT_REGISTRY):$(FACT_TAG)-$(FACT_ARCH)
+FACT_IMAGE_NAME ?= $(FACT_REGISTRY):$(FACT_TAG)
