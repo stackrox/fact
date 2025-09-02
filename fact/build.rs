81f44deeb9d9d0a1a94acbe3842116c072dee2d1
Giles Hutton <ghutton@redhat.com>
2025-09-02 11:23:56 +0100

    Cleanup/Misc changes from PR review
    Co-authored-by: Mauro Ezequiel Moltrasio <mmoltras@redhat.com>

--- fact/build.rs
+++ fact/build.rs
@@ -5,13 +5,13 @@ use std::{
     process::Command,
 };
 
-fn compile_bpf(out_dir: &Path, arch: &str) -> anyhow::Result<()> {
+fn compile_bpf(out_dir: &Path) -> anyhow::Result<()> {
     let obj = match out_dir.join("main.o").into_os_string().into_string() {
         Ok(s) => s,
         Err(os_string) => anyhow::bail!("Failed to convert path to string {:?}", os_string),
     };
 
-    let target_arch = format!("-D__TARGET_ARCH_{}", arch);
+    let target_arch = format!("-D__TARGET_ARCH_{}", env::var("CARGO_CFG_TARGET_ARCH")?);
 
     match Command::new("clang")
         .args([
@@ -55,7 +55,6 @@ fn main() -> anyhow::Result<()> {
     let out_dir: PathBuf = env::var("OUT_DIR")
         .context("Failed to interpret OUT_DIR environment variable")?
         .into();
-    let arch = env::var("CARGO_CFG_TARGET_ARCH")?;
-    compile_bpf(&out_dir, &arch).context("Failed to compile eBPF")?;
+    compile_bpf(&out_dir).context("Failed to compile eBPF")?;
     generate_bindings(&out_dir)
 }
