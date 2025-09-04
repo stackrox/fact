4915f79c063aaad6f08c920f6fbb4fa700638d7a
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-09-04 10:59:02 +0200

    Simplify kernel metric aggregation

--- fact/src/bpf/bindings.rs
+++ fact/src/bpf/bindings.rs
@@ -6,13 +6,6 @@ use libc::memcpy;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 impl path_cfg_t {
-    pub fn new() -> Self {
-        path_cfg_t {
-            path: [0; 4096],
-            len: 0,
-        }
-    }
-
     pub fn set(&mut self, path: &str) {
         let len = path.len();
         unsafe {
@@ -27,39 +20,26 @@ impl path_cfg_t {
     }
 }
 
-impl Default for path_cfg_t {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 unsafe impl Pod for path_cfg_t {}
 
-impl metrics_t {
-    pub fn accumulate(&self, other: &metrics_t) -> metrics_t {
-        let mut m = metrics_t {
-            file_open: self.file_open,
-        };
+impl metrics_by_type_t {
+    fn accumulate(&self, other: &metrics_by_type_t) -> metrics_by_type_t {
+        let mut m = metrics_by_type_t { ..*self };
 
-        m.file_open.total += other.file_open.total;
-        m.file_open.added += other.file_open.added;
-        m.file_open.dropped += other.file_open.dropped;
-        m.file_open.ignored += other.file_open.ignored;
+        m.total += other.total;
+        m.added += other.added;
+        m.dropped += other.dropped;
+        m.ignored += other.ignored;
 
         m
     }
 }
 
-impl Default for metrics_t {
-    fn default() -> Self {
-        metrics_t {
-            file_open: metrics_by_type_t {
-                total: 0,
-                added: 0,
-                dropped: 0,
-                ignored: 0,
-            },
-        }
+impl metrics_t {
+    pub fn accumulate(&self, other: &metrics_t) -> metrics_t {
+        let mut m = metrics_t { ..*self };
+        m.file_open = m.file_open.accumulate(&other.file_open);
+        m
     }
 }
 
