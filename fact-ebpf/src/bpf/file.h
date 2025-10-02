a289b613c97f0550544f046bb79e52b3fddd8ba7
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-10-02 14:56:44 +0200

    Rename path_cfg_helper_t to bound_path_t
    Also moved some logic for interacting with bound_path_t to a separate
    bound_path.h header.

--- fact-ebpf/src/bpf/file.h
+++ fact-ebpf/src/bpf/file.h
@@ -3,6 +3,7 @@
 // clang-format off
 #include "vmlinux.h"
 
+#include "bound_path.h"
 #include "builtins.h"
 #include "types.h"
 #include "maps.h"
@@ -10,57 +11,6 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 // clang-format on
-
-#define PATH_MAX_MASK (PATH_MAX - 1)
-#define path_len_clamp(len) ((len) & PATH_MAX_MASK)
-
-__always_inline static char* path_safe_access(char* p, unsigned int offset) {
-  return &p[path_len_clamp(offset)];
-}
-
-__always_inline static void path_write_char(char* p, unsigned int offset, char c) {
-  *path_safe_access(p, offset) = c;
-}
-
-__always_inline static struct path_cfg_helper_t* path_read(struct path* path) {
-  struct path_cfg_helper_t* prefix_helper = get_prefix_helper();
-
-  prefix_helper->len = bpf_d_path(path, prefix_helper->path, PATH_MAX);
-  if (prefix_helper->len <= 0) {
-    return NULL;
-  }
-
-  // Ensure length is within PATH_MAX for the verifier
-  prefix_helper->len = path_len_clamp(prefix_helper->len);
-
-  return prefix_helper;
-}
-
-enum path_append_status_t {
-  PATH_APPEND_SUCCESS = 0,
-  PATH_APPEND_INVALID_LENGTH,
-  PATH_APPEND_READ_ERROR,
-};
-
-__always_inline static enum path_append_status_t path_append_dentry(struct path_cfg_helper_t* path, struct dentry* dentry) {
-  struct qstr d_name;
-  BPF_CORE_READ_INTO(&d_name, dentry, d_name);
-  int len = d_name.len;
-  if (len + path->len > PATH_MAX) {
-    path->len += len;
-    return PATH_APPEND_INVALID_LENGTH;
-  }
-
-  char* path_offset = path_safe_access(path->path, path->len);
-  if (bpf_probe_read_kernel(path_offset, path_len_clamp(len), d_name.name)) {
-    return PATH_APPEND_READ_ERROR;
-  }
-
-  path->len += len;
-  path_write_char(path->path, path->len, '\0');
-
-  return 0;
-}
 
 /**
  * Reimplementation of the kernel d_path function.
@@ -173,7 +123,7 @@ __always_inline static char* get_host_path(char buf[PATH_MAX * 2], struct dentry
   return &buf[offset];
 }
 
-__always_inline static bool is_monitored(struct path_cfg_helper_t* path) {
+__always_inline static bool is_monitored(struct bound_path_t* path) {
   if (!filter_by_prefix) {
     // no path configured, allow all
     return true;
