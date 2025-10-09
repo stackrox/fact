8ad22415c00550d82c2738a448e822ee978b6887
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-10-09 15:06:27 +0200

    ROX-30302: implement heap profiler
    This new heap profiler is based on jemalloc and directly handled on the
    endpoints module. The implementation is not complete and requires we
    replace the allocator we use with jemalloc which has been archived a few
    months ago, so we still need to have a talk with the wider team to
    decide whether this is something we want to pursue.
    
    For cleanness, the feature is behind a compile time feature, so if we
    decide to go with this implementation, we can at least decide when we
    want to replace the default allocator.

--- fact/src/main.rs
+++ fact/src/main.rs
@@ -1,5 +1,13 @@
 use fact::config::FactConfig;
 
+#[cfg(feature = "jemalloc")]
+#[global_allocator]
+static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
+#[allow(non_upper_case_globals)]
+#[export_name = "malloc_conf"]
+pub static malloc_conf: &[u8] = b"prof:true,prof_active:false\0";
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     fact::init_log()?;
