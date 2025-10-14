6f622a81989cc43b068a3ea6343a86fa63a57c08
Dmitrii Dolgov <9erthalion6@gmail.com>
2025-10-14 13:49:36 +0200

    Add userspace memory, and kernel space metrics

--- performance-tests/main.py
+++ performance-tests/main.py
@@ -42,25 +42,66 @@ class Workload(Enum):
 
 
 def start_monitoring(args):
+    def extract_cpu_usage(output):
+        logger.debug("Extract CPU {}".format(output))
+
+        data = json.loads(output)
+        return data['run_time_ns'] / data['run_cnt']
+
+    # identify if the bpf map, as returned by bpftool, belongs to fact
+    def fact_map(data):
+        logger.debug("Filter {}".format(data))
+
+        if 'pids' not in data:
+            return False
+
+        return any((True
+            for c in data['pids']
+            if c['comm'] == 'fact'
+        ))
+
+    def extract_memory_usage(output):
+        logger.debug("Extract Memory {}".format(output))
+
+        data = json.loads(output)
+        fact_maps = filter(fact_map, data)
+        return sum(m['bytes_memlock'] for m in fact_maps)
+
     match args.metric:
         case Metric.KERNEL_SPACE_CPU:
-            args_str = 'bpftool prog show name {} --json | jq \'(.run_time_ns / .run_cnt)\''.format(
+            args_str = 'bpftool prog show name {} --json'.format(
                 'trace_file_open'
             )
+            proc_fn = extract_cpu_usage
+
         case Metric.KERNEL_SPACE_MEM:
-            return
+            args_str = 'bpftool map --json'
+            proc_fn = extract_memory_usage
 
         case Metric.USER_SPACE_CPU | Metric.USER_SPACE_MEM:
             # Monitoring for userspace is done via launching fact under the
             # monitoring tool, e.g. perf or valgrind. Hence it's noop here.
             return
 
-    args = shlex.split(args_str)
-    with open('bpf.cpu.data', 'w+') as data:
-        while True:
-            p = subprocess.Popen(args, stdout=data)
-            p.wait()
-            time.sleep(1)
+    process_args = shlex.split(args_str)
+
+    while True:
+        logger.debug(f'Starting a process: {args_str}')
+        p = subprocess.Popen(process_args, stdout=subprocess.PIPE)
+        output, errors = p.communicate()
+
+        if errors:
+            logger.debug("Could not spin monitor: {}".format(errors))
+
+        if not output:
+            continue
+
+        logger.debug("Monitor results: {}".format(output))
+
+        with open('{}.data'.format(args.metric.value), 'a') as data:
+            data.write("{}\n".format(proc_fn(output)))
+
+        time.sleep(1)
 
 
 def start_berserker(args):
@@ -75,6 +116,21 @@ def start_berserker(args):
 # Spin up fact process, if needed wrapped into the monitoring tool, e.g. perf
 # or valgrind.
 def start_fact(args):
+    def stop_perf(proc):
+        # Somehow SIGINT is not propagated to perf, so just brutally stop fact to
+        # trigger output.
+        subprocess.Popen(['pkill', 'fact'])
+
+        # Give it a moment to write the data. Perf will exit after the payload
+        # process has finished.
+        proc.wait()
+
+    def stop_valgrind(proc):
+        proc.terminate()
+
+    def stop_fact(proc):
+        proc.terminate()
+
     match args.metric:
         case Metric.USER_SPACE_CPU:
             args_str = 'perf stat -D {} -j -e {} -o {} -- fact {}'.format(
@@ -83,20 +139,25 @@ def start_fact(args):
                 'output.json',
                 args.fact_cmdline
             )
+            stop_fn = stop_perf
 
         case Metric.USER_SPACE_MEM:
-            return
+            args_str = 'valgrind --tool=massif --massif-out-file={} fact'.format(
+                'output.data',
+                args.fact_cmdline
+            )
+            stop_fn = stop_valgrind
 
         case Metric.KERNEL_SPACE_CPU | Metric.KERNEL_SPACE_MEM:
             # Monitoring for kernel space is done via launching a monitoring
             # tool in parallel with fact. Hence it's noop here.
             args_str = 'fact {}'.format(args.fact_cmdline)
-            return
+            stop_fn = stop_fact
 
     logger.debug(f'Starting a process: {args_str}')
     args = shlex.split(args_str)
 
-    return subprocess.Popen(args)
+    return subprocess.Popen(args, stdout=subprocess.DEVNULL), stop_fn
 
 
 def process_results(args):
@@ -104,42 +165,96 @@ def process_results(args):
         case Metric.USER_SPACE_CPU:
             output = json.load(open('output.json', 'r'))
 
-            return json.dumps({
+            return json.dumps([{
                 'metric': args.metric.value,
                 'workload': args.workload.value,
                 'duration': args.duration,
                 'timestamp': datetime.now(UTC).isoformat(),
                 'value': output['metric-value'],
                 'unit': output['metric-unit'],
-            })
+            }])
+
+        case Metric.USER_SPACE_MEM:
+            def heap(line):
+                return line.startswith('mem_heap_B')
+
+            def get_heap_value(line):
+                return int(line.split(sep='=')[1])
+
+            heap_lines = filter(heap, open('output.data', 'r'))
+            heap_values = list(map(get_heap_value, heap_lines))
+
+            timestamp = datetime.now(UTC).isoformat()
+
+            return json.dumps([{
+                'metric': args.metric.value,
+                'workload': args.workload.value,
+                'duration': args.duration,
+                'timestamp': timestamp,
+                'value': heap_values,
+                'unit': 'bytes',
+            }])
+
+        case Metric.KERNEL_SPACE_CPU:
+            def convert(line):
+                return float(line)
+
+            data = open('{}.data'.format(args.metric.value), 'r')
+            values = list(map(convert, data))
+            timestamp = datetime.now(UTC).isoformat()
+
+            return json.dumps([{
+                'metric': args.metric.value,
+                'workload': args.workload.value,
+                'duration': args.duration,
+                'timestamp': timestamp,
+                'value': values,
+                'unit': 'CPU utilization',
+            }])
+
+        case Metric.KERNEL_SPACE_MEM:
+            def convert(line):
+                return int(line)
+
+            data = open('{}.data'.format(args.metric.value), 'r')
+            values = list(map(convert, data))
+            timestamp = datetime.now(UTC).isoformat()
+
+            return json.dumps([{
+                'metric': args.metric.value,
+                'workload': args.workload.value,
+                'duration': args.duration,
+                'timestamp': timestamp,
+                'value': values,
+                'unit': 'bytes',
+            }])
 
 
 def main(args):
     logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO').upper())
 
-    fact = start_fact(args)
+    fact, stop_fn = start_fact(args)
     berserker = start_berserker(args)
     monitor = Process(target=start_monitoring, args=(args,))
     monitor.start()
 
     time.sleep(args.duration)
 
-    # Somehow SIGINT is not propagated to perf, so just brutally stop fact to
-    # trigger output.
-    subprocess.Popen(['pkill', 'fact'])
-
-    # Give it a moment to write the data.
-    fact.wait()
+    stop_fn(fact)
 
     berserker.terminate()
     monitor.terminate()
 
-    with open(args.output_file, 'w+') as f:
+    with open(args.output_file, 'w') as f:
         logger.debug(f'Writing results to {args.output_file}')
         f.write(process_results(args))
 
     if not args.keep_traces:
-        os.unlink('output.json')
+        for trace in ('output.json', 'output.data'):
+            try:
+                os.unlink(trace)
+            except FileNotFoundError:
+                pass
 
 
 if __name__ == '__main__':
