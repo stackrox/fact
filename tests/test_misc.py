017e98cba6eef0efb559a96d1b245e9955856973
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2026-01-19 10:23:30 +0000

    feat: sanitize paths generated by calling d_path (#181)
    * feat: sanitize paths generated by calling d_path
    
    The paths generated from calling bpf_d_path can have a " (deleted)"
    suffix when a file is removed from the system, this can mess with our
    files being reported, so some basic sanitization is added to these
    buffers.
    
    * test: add test_d_path_sanitization integration test
    
    A new test is added which builds and runs a small container with a Rust
    binary that deletes itself and accesses a monitored file. This will
    trigger an open event where the executable path retrieved by the
    `bpf_d_path` helper will add a " (deleted)" suffix, the test checks our
    code correctly strips this suffix.
    
    * chore: rename parse_d_path to sanitize_d_path
    
    Also add a small paragraph on files that end with the " (deleted)"
    suffix being an unaddressed edge case.

---
+++ tests/test_misc.py
@@ -0,0 +1,67 @@
+import os
+
+from conftest import dump_logs
+from event import Event, EventType, Process
+
+import pytest
+
+
+@pytest.fixture
+def build_self_deleter(docker_client):
+    image, _ = docker_client.images.build(
+        path='containers/self-deleter',
+        tag='self-deleter:latest',
+        dockerfile='Containerfile'
+    )
+    return image
+
+
+@pytest.fixture
+def run_self_deleter(fact, monitored_dir, logs_dir, docker_client, build_self_deleter):
+    image = build_self_deleter.tags[0]
+    container = docker_client.containers.run(
+        image,
+        '/mounted/test.txt',
+        detach=True,
+        volumes={
+            monitored_dir: {
+                'bind': '/mounted',
+                'mode': 'z',
+            },
+        },
+        name='self-deleter',
+    )
+
+    yield container
+
+    container_log = os.path.join(logs_dir, 'self-deleter.log')
+    container.stop(timeout=1)
+    dump_logs(container, container_log)
+    container.remove()
+
+
+def test_d_path_sanitization(fact, monitored_dir, server, run_self_deleter, docker_client):
+    """
+    Ensure the sanitization of paths obtained by calling the bpf_d_path
+    helper don't include the " (deleted)" suffix when the file is
+    removed.
+    """
+    # File Under Test
+    fut = '/mounted/test.txt'
+    host_path = os.path.join(monitored_dir, 'test.txt')
+
+    container = run_self_deleter
+
+    process = Process(pid=None,
+                      uid=0,
+                      gid=0,
+                      exe_path='/usr/local/bin/self-deleter',
+                      args=f'self-deleter {fut}',
+                      name='self-deleter',
+                      container_id=container.id[:12],
+                      loginuid=pow(2, 32)-1)
+    event = Event(process=process, event_type=EventType.OPEN,
+                  file=fut, host_path=host_path)
+    print(f'Waiting for event: {event}')
+
+    server.wait_events([event])
