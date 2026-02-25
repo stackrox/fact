b198ad3b94b293d5e6bcf1b82b26ba2eee0905c1
Giles Hutton <ghutton@redhat.com>
2026-02-25 16:22:35 +0000

    Fix tests based on PR comments

--- tests/test_wildcard.py
+++ tests/test_wildcard.py
@@ -1,3 +1,4 @@
+from time import sleep
 import os
 
 import pytest
@@ -7,28 +8,33 @@ from event import Event, EventType, Process
 
 
 @pytest.fixture
-def wildcard_config(fact_config, monitored_dir):
+def wildcard_config(fact, fact_config, monitored_dir):
     config, config_file = fact_config
     config['paths'] = [
         f'{monitored_dir}/**/*.txt',
+        f'{monitored_dir}/*.conf',
         f'{monitored_dir}/**/test-*.log',
     ]
     with open(config_file, 'w') as f:
         yaml.dump(config, f)
+
+    # reload the config
+    fact.kill('SIGHUP')
+    sleep(0.1)
     return config, config_file
 
 
-def test_extension_wildcard(fact, wildcard_config, monitored_dir, server):
+def test_extension_wildcard(wildcard_config, monitored_dir, server):
     process = Process.from_proc()
-
-    txt_file = os.path.join(monitored_dir, 'document.txt')
-    with open(txt_file, 'w') as f:
-        f.write('This should be captured')
 
     # Should not match any pattern
     log_file = os.path.join(monitored_dir, 'app.log')
     with open(log_file, 'w') as f:
         f.write('This should be ignored')
+
+    txt_file = os.path.join(monitored_dir, 'document.txt')
+    with open(txt_file, 'w') as f:
+        f.write('This should be captured')
 
     e = Event(process=process, event_type=EventType.CREATION,
               file=txt_file, host_path='')
@@ -36,17 +42,17 @@ def test_extension_wildcard(fact, wildcard_config, monitored_dir, server):
     server.wait_events([e])
 
 
-def test_prefix_wildcard(fact, wildcard_config, monitored_dir, server):
+def test_prefix_wildcard(wildcard_config, monitored_dir, server):
     process = Process.from_proc()
-
-    test_log = os.path.join(monitored_dir, 'test-app.log')
-    with open(test_log, 'w') as f:
-        f.write('This should be captured')
 
     # Wrong prefix - should not match
     app_log = os.path.join(monitored_dir, 'app-test.log')
     with open(app_log, 'w') as f:
         f.write('This should be ignored')
+
+    test_log = os.path.join(monitored_dir, 'test-app.log')
+    with open(test_log, 'w') as f:
+        f.write('This should be captured')
 
     e = Event(process=process, event_type=EventType.CREATION,
               file=test_log, host_path='')
@@ -54,11 +60,16 @@ def test_prefix_wildcard(fact, wildcard_config, monitored_dir, server):
     server.wait_events([e])
 
 
-def test_recursive_wildcard(fact, wildcard_config, monitored_dir, server):
+def test_recursive_wildcard(wildcard_config, monitored_dir, server):
     process = Process.from_proc()
 
     nested_dir = os.path.join(monitored_dir, 'level1', 'level2')
     os.makedirs(nested_dir, exist_ok=True)
+
+    # Different extension - should not match
+    nested_md = os.path.join(nested_dir, 'readme.md')
+    with open(nested_md, 'w') as f:
+        f.write('Should be ignored')
 
     root_txt = os.path.join(monitored_dir, 'root.txt')
     with open(root_txt, 'w') as f:
@@ -67,11 +78,6 @@ def test_recursive_wildcard(fact, wildcard_config, monitored_dir, server):
     nested_txt = os.path.join(nested_dir, 'nested.txt')
     with open(nested_txt, 'w') as f:
         f.write('Nested txt')
-
-    # Different extension - should not match
-    nested_md = os.path.join(nested_dir, 'readme.md')
-    with open(nested_md, 'w') as f:
-        f.write('Should be ignored')
 
     events = [
         Event(process=process, event_type=EventType.CREATION,
@@ -83,8 +89,26 @@ def test_recursive_wildcard(fact, wildcard_config, monitored_dir, server):
     server.wait_events(events)
 
 
-def test_multiple_patterns(fact, wildcard_config, monitored_dir, server):
+def test_nonrecursive_wildcard(wildcard_config, monitored_dir, server):
     process = Process.from_proc()
+
+    fut = os.path.join(monitored_dir, 'app.conf')
+    with open(fut, 'w') as f:
+        f.write('This should be captured')
+
+    e = Event(process=process, event_type=EventType.CREATION,
+              file=fut, host_path='')
+
+    server.wait_events([e])
+
+
+def test_multiple_patterns(wildcard_config, monitored_dir, server):
+    process = Process.from_proc()
+
+    # Matches no pattern
+    conf_file = os.path.join(monitored_dir, 'config.yml')
+    with open(conf_file, 'w') as f:
+        f.write('Config file')
 
     txt_file = os.path.join(monitored_dir, 'notes.txt')
     with open(txt_file, 'w') as f:
@@ -93,11 +117,6 @@ def test_multiple_patterns(fact, wildcard_config, monitored_dir, server):
     log_file = os.path.join(monitored_dir, 'test-output.log')
     with open(log_file, 'w') as f:
         f.write('Log file')
-
-    # Matches neither pattern
-    conf_file = os.path.join(monitored_dir, 'config.yml')
-    with open(conf_file, 'w') as f:
-        f.write('Config file')
 
     events = [
         Event(process=process, event_type=EventType.CREATION,
