00be062b18ed80d66cf228bc233c9f7f33f9e353
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2026-07-20 17:01:44 +0200

    fix(host_scanner): reduce the amount of times stat is called per scan
    Instead of using `PathBuf::is_file`, `PathBuf::is_dir` and
    `PathBuf::metadata` individually, each of which produce independent
    calls to `stat` under the hood, use a single call to `PathBuf::metadata`
    and propagate it down to the places it is needed.

--- fact/src/host_scanner.rs
+++ fact/src/host_scanner.rs
@@ -21,6 +21,7 @@
 use std::{
     cell::RefCell,
     collections::HashMap,
+    fs::Metadata,
     io,
     ops::{Deref, DerefMut},
     os::linux::fs::MetadataExt,
@@ -198,14 +199,28 @@ impl HostScanner {
         };
 
         for entry in glob::glob(glob_str)? {
-            let path = entry?;
-            if path.is_file() {
+            let path = match entry {
+                Ok(p) => p,
+                Err(e) => {
+                    debug!("Glob expansion failed: {e:?}");
+                    continue;
+                }
+            };
+            let metadata = match path.metadata() {
+                Ok(p) => p,
+                Err(e) => {
+                    debug!("Failed to get metadata for {}: {e:?}", path.display());
+                    continue;
+                }
+            };
+
+            if metadata.is_file() {
                 self.metrics.scan_inc(ScanLabels::FileScanned);
-                self.update_entry(path.as_path())
+                self.update_entry(path.as_path(), &metadata)
                     .with_context(|| format!("Failed to update entry for {}", path.display()))?;
-            } else if path.is_dir() {
+            } else if metadata.is_dir() {
                 self.metrics.scan_inc(ScanLabels::DirectoryScanned);
-                self.update_entry(path.as_path())
+                self.update_entry(path.as_path(), &metadata)
                     .with_context(|| format!("Failed to update entry for {}", path.display()))?;
             } else {
                 self.metrics.scan_inc(ScanLabels::FsItemIgnored);
@@ -214,14 +229,7 @@ impl HostScanner {
         Ok(())
     }
 
-    fn update_entry(&self, path: &Path) -> anyhow::Result<()> {
-        if !path.exists() {
-            // If path does not exist, we don't have anything to update
-            self.metrics.scan_inc(ScanLabels::FileRemoved);
-            return Ok(());
-        }
-
-        let metadata = path.metadata()?;
+    fn update_entry(&self, path: &Path, metadata: &Metadata) -> anyhow::Result<()> {
         let inode = inode_key_t {
             inode: metadata.st_ino(),
             dev: metadata.st_dev(),
