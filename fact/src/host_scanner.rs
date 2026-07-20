69cf6dac57dae3763a665858c4e003ba16a99e35
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2026-07-20 12:25:32 +0200

    chore: print scan duration

--- fact/src/host_scanner.rs
+++ fact/src/host_scanner.rs
@@ -39,6 +39,7 @@ use log::{debug, info, warn};
 use tokio::{
     sync::{Notify, mpsc, watch},
     task::JoinHandle,
+    time::Instant,
 };
 
 use io_uring::{IoUring, opcode, types};
@@ -132,7 +133,8 @@ impl HostScanner {
     }
 
     fn scan(&self) -> anyhow::Result<()> {
-        debug!("Host scan started");
+        info!("Host scan started");
+        let start = Instant::now();
         self.metrics.scan_inc(ScanLabels::Scans);
         let config = self.paths.borrow();
 
@@ -156,7 +158,11 @@ impl HostScanner {
             let path = host_info::prepend_host_mount(pattern);
             self.scan_inner(&path)?;
         }
-        debug!("Host scan done");
+        let duration = start.elapsed();
+        info!(
+            "Host scan done, took {duration:?}. Inodes tracked: {}",
+            self.inode_map.borrow().len()
+        );
 
         Ok(())
     }
