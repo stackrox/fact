4da2e85e9352f7bcab979cc4092847e88b5e2296
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2026-07-30 17:47:14 +0200

    chore: better log time slice for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import json
 import os
-from datetime import datetime, timezone
+import time
 from shutil import rmtree
 from tempfile import NamedTemporaryFile, mkdtemp
 from time import sleep
@@ -298,6 +298,8 @@ def fact_config(
     fact does not fire on directory cleanup, and captures
     timestamp-sliced container logs for the test.
     """
+    test_start = time.time()
+
     config = base_config()
     if server.output_mode == 'otlp':
         config['otel'] = {'endpoint': 'http://127.0.0.1:4318/v1/logs'}
@@ -313,12 +315,9 @@ def fact_config(
     server.drain()
     reload_fact(fact, config, fact_config_file)
 
-    test_start = datetime.now(tz=timezone.utc)
     metrics_before = MetricsSnapshot.fetch(ENDPOINT_ADDRESS)
 
     yield config, fact_config_file
-
-    test_end = datetime.now(tz=timezone.utc)
 
     with (
         open(os.path.join(logs_dir, 'fact.yml'), 'w') as out,
@@ -335,13 +334,14 @@ def fact_config(
         pass
 
     try:
+        test_end = time.time()
         log_bytes = fact.logs(since=test_start, until=test_end)
         with open(os.path.join(logs_dir, 'fact.log'), 'wb') as f:
             f.write(log_bytes)
     except Exception:
         pass
 
-    config['paths'] = []
+    config = base_config()
     reload_fact(fact, config, fact_config_file)
 
 
