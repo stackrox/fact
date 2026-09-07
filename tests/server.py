7e2b2a949e289c35f9b2684943937fdef72b6e6e
Olivier Valentin <ovalenti@redhat.com>
2026-09-07 18:58:11 +0200

    Add mapping for link events

--- tests/server.py
+++ tests/server.py
@@ -41,6 +41,7 @@ if TYPE_CHECKING:
 EVENT_TYPE_MAP: dict[str, EventType] = {
     'open': EventType.OPEN,
     'creation': EventType.CREATION,
+    'link': EventType.CREATION,
     'unlink': EventType.UNLINK,
     'permission': EventType.PERMISSION,
     'ownership': EventType.OWNERSHIP,
